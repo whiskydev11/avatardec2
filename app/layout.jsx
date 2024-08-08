2c9ad975ad4c7fce718278ae113e2df19d1442c3
Whisky Schwarz <77167492+whiskydev11@users.noreply.github.com>
2024-08-08 22:32:04 +0300

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,26 +10,25 @@ export default function RootLayout({ children }) {
 		<html lang="en">
 			<head>
 				{/* Meta tags */}
-				<title>Discord Fake Avatar Decorations</title>
-				<meta name="description" content="Get Discord avatar decorations for free without spending money!" />
+				<title>Discord Sahte Avatar Dekorasyonları</title>
+				<meta name="description" content="Para harcamadan Discord avatar dekorasyonlarını ücretsiz edinin!" />
 
 				{/* Open Graph embeds */}
 				<meta property="og:type" content="website" />
-				<meta property="og:title" content="Discord Fake Avatar Decorations" />
-				<meta property="og:description" content="Get Discord avatar decorations for free without spending money!" />
+				<meta property="og:title" content="Discord Sahte Avatar Dekorasyonları" />
+				<meta property="og:description" content="Para harcamadan Discord avatar dekorasyonlarını ücretsiz edinin!" />
 				<meta property="og:image" content="https://discord-decorations.vercel.app/android-chrome-192x192.png" />
 
 				{/* Twitter embeds */}
 				<meta name="twitter:card" content="summary" />
-				<meta name="twitter:title" content="Discord Fake Avatar Decorations" />
-				<meta name="twitter:description" content="Get Discord avatar decorations for free without spending money!" />
+				<meta name="twitter:title" content="Discord Sahte Avatar Dekorasyonları" />
+				<meta name="twitter:description" content="Para harcamadan Discord avatar dekorasyonlarını ücretsiz edinin!" />
 				<meta property="twitter:image" content="https://discord-decorations.vercel.app/android-chrome-192x192.png" />
 
 				{/* Favicon */}
 				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
 				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-				<link rel="manifest" href="/site.webmanifest" />
 
 				{/* Fonts */}
 				<link rel="stylesheet" href="https://itspi3141.github.io/discord-fonts/font.css" />
